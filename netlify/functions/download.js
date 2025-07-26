a0220f3580f83f049a6e749da9aa13b1f9b2843d
Shalini Ananda <50239203+ShaliniAnandaPhD@users.noreply.github.com>
2025-07-25 20:14:07 -0700

    Update download.js

--- netlify/functions/download.js
+++ netlify/functions/download.js
@@ -1,15 +1,18 @@
 // netlify/functions/download.js
-const { getStore } = require('@netlify/blobs');
+import { getStore } from '@netlify/blobs';
 
-exports.handler = async (event) => {
+export default async (event, context) => {
+    // Get the job ID from the query string parameters.
     const jobId = event.queryStringParameters.id;
     const store = getStore('audio_uploads');
 
     try {
+        // Retrieve the generated zip file from the blob store.
         const zipData = await store.get(`${jobId}-result`, { type: 'buffer' });
         if (!zipData) {
             return { statusCode: 404, body: 'Result not found.' };
         }
+        // Return the zip file to the client.
         return {
             statusCode: 200,
             headers: { 'Content-Type': 'application/zip' },
